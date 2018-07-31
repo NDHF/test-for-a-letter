3df28f9292f1541d9bd7a5d7c94e711f2131316d
Nicholas Bernhard <nicholas@NDHFilms.com>
2018-07-30 22:54:26 -0600

    Initial commit

---
+++ code.js
@@ -0,0 +1,29 @@
+// This function will check if the input is a character in the alphabet.
+// It will incorporate seamlessly with your own code, and make it safer
+// in the process. 
+
+function testForLetter(character) {
+    const alphabet = 
+    ["A", "a", "B", "b", "C", "c", "D", "d", "E", "e", "F", "f", "G", "g", 
+     "H", "h", "I", "i", "J", "j", "K", "k", "L", "l", "M", "m", "N", "n",
+     "O", "o", "P", "p", "Q", "q", "R", "r", "S", "s", "T", "t", "U", "u",
+     "V", "v", "W", "w", "X", "x", "Y", "y", "Z", "z"
+    ];
+
+    if (alphabet.includes(character)) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+function yourFunctionNameHere(character) {
+// Wrap your code in this if-else statement. The idea is to pass your function's
+// argument through the testForLetter function. 
+    if (testForLetter(character) === false) {
+        console.error("Please enter a letter in the alphabet");
+    } else {
+        console.log("Input is a letter in the alphabet");
+        //Place your code here!!!
+    }
+}
